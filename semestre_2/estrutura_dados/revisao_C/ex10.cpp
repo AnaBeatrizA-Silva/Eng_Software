767d2c118a6e0731d2cdd7dd086ff3dd64b517b3
Ana Beatriz <ana.beatriz.adriano.silva.1929@gmail.com>
2025-08-19 20:04:48 -0300

    Exercicio 10 da revisao de C em estrutura de dados

--- semestre_2/estrutura_dados/revisao_C/ex10.cpp
+++ semestre_2/estrutura_dados/revisao_C/ex10.cpp
@@ -11,6 +11,7 @@ Análise de Produção – Linha de Montagem: Uma fábrica registra a produção em uma 
 int main(void) {
 	
 	float m[4][4];
+	int sum = 0;
 //	int i, j;
 	
 	for(int i = 0; i < 4; i++) {
@@ -22,12 +23,28 @@ int main(void) {
 		}
 	}
 	
+	// mostra a matriz na tela
 	for(int i = 0; i < 4; i++) {
 		printf("\n");
 		for(int j = 0; j < 4; j++) {
-			printf("%f", m[i][j]);
+			printf("%.1f  \n", m[i][j]);
 		}
 	}
+	
+	// calcula a diagonal principal
+	
+	/*
+		00 01 02 03
+		10 11 12 13
+		20 21 22 23
+		30 31 32 33
+	*/
+	
+	for(int i = 0; i < 4; i++) {
+		sum += m[i][i];
+	}
+
+	printf("Total da diagonal principal e: %i", sum);
 
 	return 0;
 }
