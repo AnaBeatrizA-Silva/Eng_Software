d5190490d61fe03dd6fb9e4f9706a365d15aadb4
Ana Beatriz Adriano da Silva <ana.beatriz.adriano.silva.1929@gmail.com>
2025-08-20 19:03:22 -0300

    Adicionando a atividade de POO

---
+++ semestre_2/POO/sistemaBiblioteca/Biblioteca.Console/Program.cs
@@ -0,0 +1,23 @@
+﻿// Seção do programa principal]
+
+int op;
+
+while (true)
+{
+	Console.Clear();
+	Console.WriteLine("---=== Menu ===---");
+	Console.WriteLine("1 - Cadastrar Livro");
+	Console.WriteLine("2 - Consultar Livro");
+	Console.WriteLine("3 - Excluir Livro");
+	Console.WriteLine("4 - Relatório de Livros");
+	Console.WriteLine("0 - Sair");
+	Console.WriteLine("Opção desajada: ");
+	op = int.Parse(Console.ReadLine());
+
+	if (op == 0) break;
+
+	if (op == 1) cadastrarLivro();
+}
+
+
+// Seção das funções
