1a964fffa2d5f0e317d04b4e2ecf9994eb8f4b13
Ana Beatriz Adriano da Silva <ana.beatriz.adriano.silva.1929@gmail.com>
2025-08-27 22:25:01 -0300

    Criação da primeira classe chamad Livro

---
+++ semestre_2/POO/sistemaBiblioteca/Biblioteca.Console/Livro.cs
@@ -0,0 +1,15 @@
+using System.Diagnostics.Contracts;
+
+public class Livro
+{
+    // Propriedades
+    public int codigo;
+    public string titulo;
+    public string autor;
+    public int paginas;
+    public double preco;
+
+
+
+    // Métodos
+}
--- semestre_2/POO/sistemaBiblioteca/Biblioteca.Console/Program.cs
+++ semestre_2/POO/sistemaBiblioteca/Biblioteca.Console/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+
+Cada classe em um arquivo, cada arquivo com apenas uma classe
+
+Arquivo e classe com o mesmo nome.
+
+*/
+
+Console.Clear();
+
+Console.WriteLine("---=== Sistema de Biblioteca ---===");
+
+// Cria o objeto 'livro1'
+Livro livro1 = new Livro();
+
+// define os valores das propriedadees para 'livro1'
+livro1.codigo = 67;
+livro1.titulo = "Senhor do Anéis";
+livro1.autor = "JRR Tolkien";
+livro1.paginas = 1270;
+livro1.preco = 120.80;
+
+// Cria o objeto 'livro2'
+Livro livro2 = new Livro();
+
+// define os valores das propriedadees para 'livro2'
+livro2.codigo = 11;
+livro2.titulo = "Harry Potter";
+livro2.autor = "JK Rowling";
+livro2.paginas = 350;
+livro2.preco = 70.65;
+
+// Imprime os livros
+
+Console.WriteLine(livro1.titulo);
+Console.WriteLine(livro2);
+
