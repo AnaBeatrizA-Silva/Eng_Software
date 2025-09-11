10241e7df703ebe13345842f215bd0285e267e98
Ana Beatriz Adriano da Silva <ana.beatriz.adriano.silva.1929@gmail.com>
2025-09-10 21:18:16 -0300

    Aula sobre orientação a objetos

--- semestre_2/POO/sistemaBiblioteca/Biblioteca.Console/Livro.cs
+++ semestre_2/POO/sistemaBiblioteca/Biblioteca.Console/Livro.cs
@@ -1,5 +1,6 @@
 using System.Diagnostics.Contracts;
 
+
 public class Livro
 {
     // Propriedades
@@ -10,6 +11,25 @@ public class Livro
     public double preco;
 
 
-
     // Métodos
+
+    public Livro(int cod, string tit, string alt, int pag, double prc)
+    {
+        this.codigo = cod;
+        this.titulo = tit;
+        this.autor = alt;
+        this.paginas = pag;
+        this.preco = prc;
+    }
+
+    public void ImprimirLivro()
+    {
+        Console.WriteLine($"--- Dados do Livro {this.codigo} ---");
+        Console.WriteLine($"Codigo: {this.codigo}"); // no c# não é obrigatório usar
+        Console.WriteLine($"Título: {this.titulo}");
+        Console.WriteLine($"Autor: {this.autor}");
+        Console.WriteLine($"Números de Páginas: {this.paginas}");
+        Console.WriteLine($"Preço: {this.preco}");
+        Console.WriteLine("");
+    }
 }
--- semestre_2/POO/sistemaBiblioteca/Biblioteca.Console/Program.cs
+++ semestre_2/POO/sistemaBiblioteca/Biblioteca.Console/Program.cs
@@ -6,32 +6,20 @@ Arquivo e classe com o mesmo nome.
 
 */
 
+List<Livro> livros = new List<Livro>();
+
 Console.Clear();
 
 Console.WriteLine("---=== Sistema de Biblioteca ---===");
 
 // Cria o objeto 'livro1'
-Livro livro1 = new Livro();
-
-// define os valores das propriedadees para 'livro1'
-livro1.codigo = 67;
-livro1.titulo = "Senhor do Anéis";
-livro1.autor = "JRR Tolkien";
-livro1.paginas = 1270;
-livro1.preco = 120.80;
+Livro livro1 = new Livro(67, "Senhor do Anéis","JRR Tolkien", 1270, 120.80);
 
 // Cria o objeto 'livro2'
-Livro livro2 = new Livro();
-
-// define os valores das propriedadees para 'livro2'
-livro2.codigo = 11;
-livro2.titulo = "Harry Potter";
-livro2.autor = "JK Rowling";
-livro2.paginas = 350;
-livro2.preco = 70.65;
+Livro livro2 = new Livro(11, "Harry Potter", "JK Rowling", 350, 70.65);
 
 // Imprime os livros
+livro1.ImprimirLivro();
+livro2.ImprimirLivro();
 
-Console.WriteLine(livro1.titulo);
-Console.WriteLine(livro2);
-
+Console.ReadKey();  
