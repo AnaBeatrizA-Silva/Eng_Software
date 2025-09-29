dc8d00e4d423e7229aa632b4bc590d23e31091e2
Ana Beatriz <ana.beatriz.adriano.silva.1929@gmail.com>
2025-09-28 22:12:54 -0300

    .

---
+++ estudos/POO/ExerciciosPOO/Nivel_1/Produto.cs
@@ -0,0 +1,6 @@
+public class Produto
+{
+    public string nome;
+    public string ctg;
+    public double prc;
+}
---
+++ estudos/POO/ExerciciosPOO/Nivel_1/Program.cs
@@ -0,0 +1,33 @@
+﻿Console.Clear();
+
+/* Ex 1
+Produto produto1 = new Produto();
+
+produto1.nome = "Rappoo VT7 Pro";
+produto1.ctg = "Mouse";
+produto1.prc = 300;
+
+Console.WriteLine(produto1.nome);
+Console.WriteLine(produto1.ctg);
+Console.WriteLine(produto1.prc);
+*/
+/* Ex 2
+ContaBancaria conta1 = new ContaBancaria();
+
+conta1.titular = "Ana";
+conta1.Depositar(200);
+conta1.Sacar(400);
+
+Console.WriteLine("Saldo e R$" + conta1.ObterSaldo());
+*/
+
+/* Ex 3
+Funcionario f1 = new Funcionario();
+Funcionario f2 = new Funcionario("Joao", 23);
+Funcionario f3 = new Funcionario("Maria", 43, 600);
+
+Console.WriteLine($"{f1.nome}, {f1.idade}, {f1.salario}");
+Console.WriteLine($"{f2.nome}, {f2.idade}, {f2.salario}");
+Console.WriteLine($"{f3.nome}, {f3.idade}, {f3.salario}");
+*/
+
